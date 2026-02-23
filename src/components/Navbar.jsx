8303df8dfd48c4c32c3d4afe43adb3a87df110cb
mhdalfachrozi <mhdalfachrozi31@gmail.com>
2026-02-23 18:08:22 +0700

    about us section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 // React Icons
 import { FiMenu, FiHome, FiUsers } from "react-icons/fi";
-import { PiMedal, PiRocket } from "react-icons/pi";
+import { PiMedal, PiRocket, PiRocketLaunch } from "react-icons/pi";
 import { IoIosArrowDown } from "react-icons/io";
+import { MdOutlineRecycling } from "react-icons/md";
+import { GiCoffeeBeans } from "react-icons/gi";
 
 const Navbar = () => {
     // TODO: handle click for mobile menu
@@ -14,10 +16,19 @@ const Navbar = () => {
 
     let menuActiveClass = isMenuOpen ? "-left-10" : "-left-full";
 
+    // TODO: handle click for dropdown menu
+    const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
+
+    const dropDownClick = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    let dropDownActive = isDropdownOpen ? "flex" : "hidden";
+
     return (
     <>
-        <div className="navbar fixed w-full flex justify-center  z-50 transition-all">
-            <div className="container bg-white/45 backdrop-blur-[15px] border border-white shadow-[1px_1px_15px_rgba(0,0,0,0.25)] max-w-[90%] my-4 p-2 rounded-[20px]">
+        <div className="navbar fixed w-full flex justify-center z-50 transition-all">
+            <div className="container bg-white/45 backdrop-blur-[15px] border border-white shadow-[1px_1px_15px_rgba(0,0,0,0.25)] max-w-[85%] my-4 p-2 rounded-[20px]">
                 <div className="navbar-box flex items-center justify-between px-5 text-primary">
                     <div className="logo flex items-center gap-3">
                         <img src="public/assets/logo-pmbemunsri.webp" alt="Logo PM BEM 2025" className="w-16 h-16"/>
@@ -26,48 +37,73 @@ const Navbar = () => {
                         </h1>
                     </div> 
                     <ul className={`
-                        flex
-                        lg:gap-14 gap-8
-                        md:static
-                        md:flex-row
-                        md:w-auto
-                        md:h-full
-                        md:p-0
-                        md:transision-none
-                        fixed
-                        translate-y-60
-                        md:translate-y-0
-                        flex-col 
-                        p-8
+                        fixed md:static 
+                        flex md:flex-row  flex-col
+                        p-8 md:p-0
                         ${menuActiveClass}
+                        md:w-auto md:h-full
+                        lg:gap-14 gap-8
+                        md:transision-none
+                        translate-y-60 md:translate-y-0
                         rounded-r-[20px] 
-                        md:text-primary text-white 
-                        md:bg-transparent bg-[#4ADE80]
-                        shadow-[1px_1px_15px_rgba(0,0,0,0.25)] 
-                        md:shadow-none 
+                        bg-[#22C55E] md:bg-transparent
+                        text-white md:text-primary
+                        shadow-[1px_1px_15px_rgba(0,0,0,0.25)] md:shadow-none 
                         transition-all
                         `}>
                         <li className="flex items-center gap-3">
                             <FiHome className="text-2xl md:hidden block"/>
-                            <a href="#" className="font-medium hover:text-secondary hover:font-semibold transition-transform duration-300">Beranda</a>
+                            <a href="#beranda" className="font-medium hover:text-secondary hover:font-semibold transition-transform duration-300">Beranda</a>
                         </li>
                         <li className="flex items-center gap-3">
                             <PiMedal className="text-2xl md:hidden block"/>
-                            <a href="#" className="font-medium hover:text-secondary hover:font-semibold transition-transform duration-300">Tentang Kami</a>
+                            <a href="#tentang-kami" className="font-medium hover:text-secondary hover:font-semibold transition-transform duration-300">Tentang Kami</a>
                         </li>
                         <li className="flex items-center gap-3">
                             <FiUsers className="text-2xl md:hidden block"/>
                             <a href="#" className="font-medium hover:text-secondary hover:font-semibold transition-transform duration-300">Profile</a>
                         </li>
-                         <li className="flex items-center gap-3">
+                        <li className="relative flex flex-col md:flex-row items-center gap-3">
+                            <div className="flex flex-row gap-3 w-full">
                             <PiRocket className="text-2xl md:hidden block"/>
-                            <a href="#" className="flex items-center gap-1 font-medium hover:text-secondary hover:font-semibold transition-transform duration-300">Program Kerja
-                                <IoIosArrowDown className="text-xl" />
-                            </a>
+                            <button onClick={dropDownClick} className="flex items-center gap-1 font-medium hover:text-secondary hover:font-semibold transition-transform duration-300">
+                                Program Kerja 
+                                <IoIosArrowDown className={`
+                                    transition-transform duration-300 
+                                    ${isDropdownOpen ? "rotate-180" : ""}
+                                    `} />
+                            </button>
+                            </div>
+
+                            {/* Dropdown Program Kerja Menu */}
+                            <ul className={`
+                                md:absolute flex flex-col gap-3
+                                md:flex ${dropDownActive}
+                                left-5
+                                md:mt-44 w-max md:p-5 
+                                rounded-xl
+                                md:bg-[#22C55E] text-white
+                                md:shadow-lg
+                                transition-all duration-300
+                                ${
+                                isDropdownOpen
+                                    ? "opacity-100 scale-100 translate-y-0"
+                                    : "opacity-0 scale-95 -translate-y-2 pointer-events-none"
+                                }
+                            `}>
+                                <li className="flex flex-row items-center gap-2 font-normal text-base">
+                                    <GiCoffeeBeans />
+                                    <a href="">Home Dry Coffee</a>
+                                </li>
+                                <li className="flex flex-row gap-2 font-normal text-base">
+                                    <MdOutlineRecycling className="mt-1"/>
+                                    <a href="">Pengolahan Limbah <br />Kopi</a>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
                     <div className="galeri flex items-center gap-2">
-                        <a href="#" className="px-6 py-3 rounded-[25px] bg-gradient-to-r from-[#22C55E] to-[#4ADE80] hover:from-[#16A34A] hover:to-[#22C55E] text-white transition-all duration-300">Galeri</a>
+                        <a href="#" className="px-6 py-3 rounded-[25px] bg-linear-to-r from-[#22C55E] to-[#4ADE80] hover:from-[#16A34A] hover:to-[#22C55E] text-white transition-all duration-300">Galeri</a>
                         <div className="block md:hidden" onClick={handleClick}>
                             <FiMenu className="text-2xl" />
                         </div>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -12,19 +12,24 @@ import 'swiper/css/navigation';
 // import required modules
 import { Autoplay, Pagination, Navigation, EffectFade } from 'swiper/modules';
 
+// React Icons
+import { FiMenu, FiHome, FiUsers } from "react-icons/fi";
+import { PiMedal } from "react-icons/pi";
+
 const Homepage = () => {
     return (
-        <div className="homepage flex items-center justify-center w-screen bg-black">
-            <div className="container flex justify-center bg-red-500">
-                <div className="hero md:relative flex flex-col items-center justify-center md:h-screen mx-10 md:mx-0 overflow-hidden">
-                    <div className="box-hero-1 md:relative lg:block hidden bg-green-500">
+        <div className="homepage flex items-center justify-center w-full">
+            <div className="container flex flex-col items-center w-full pt-32 md:pt-0 overflow-hidden">
+                {/* Hero Section */}
+                <div id="beranda" className="hero md:relative flex flex-col items-center justify-center md:h-screen mx-5 md:mx-0 overflow-hidden">
+                    <div className="box-hero-1 md:relative lg:block hidden">
                         <Swiper
                             spaceBetween={0}
                             effect={'fade'}
                             centeredSlides={true}
                             loop={true}
                             autoplay={{
-                            delay: 2000,
+                            delay: 2300,
                             disableOnInteraction: false,
                             }}
                             pagination={{
@@ -50,20 +55,20 @@ const Homepage = () => {
                     </div>
 
                     <div className="box-hero-2 md:absolute flex flex-col items-center justify-center z-10 mb-10 w-fit">
-                        <img src="/assets/logo-pmbemunsri.webp" alt="" className="w-20 md:w-32" />
-                        <h1 className="text-center text-2xl md:text-4xl font-extrabold text-white leading-[40px] tracking-wide">
+                        <img src="/assets/logo-pmbemunsri.webp" alt="" className="w-full md:w-32" />
+                        <h1 className="text-center md:text-4xl text-2xl font-extrabold md:text-white text-primary leading-10 tracking-wide">
                             PROGRAM MAHASISWA BERDAMPAK <br />
-                        <span className="md:text-3xl">
-                            PEMBERDAYAAN MASYARAKAT OLEH <span>BADAN EKSEKUTIF MAHASISWA</span> <br />
+                        <span className="md:text-3xl text-xl">
+                            PEMBERDAYAAN MASYARAKAT OLEH <span className="italic">BADAN EKSEKUTIF MAHASISWA</span> <br />
                         </span>
-                        <span className="md:text-3xl font-light italic">
+                        <span className="md:text-3xl text-xl font-light italic">
                             UNIVERSITAS SRIWIJAYA 2025
                         </span>
                         </h1>
                     </div>
 
-                    <div className="box-hero-3 md:absolute z-10 flex flex-col gap-1 px-9 py-1 rounded-full items-center justify-center md:mt-96 w-fit bg-white/45 backdrop-blur-[15px] border border-white shadow-[1px_1px_15px_rgba(0,0,0,0.25)] max-w-[90%]">
-                        <div className="text-normal italic text-white">
+                    <div className="box-hero-3 md:absolute flex flex-col mb-5 gap-1 px-9 py-1 z-10 rounded-full items-center justify-center md:mt-96 w-fit bg-white/45 backdrop-blur-[15px] border border-white shadow-[1px_1px_15px_rgba(0,0,0,0.25)] max-w-[90%]">
+                        <div className="text-normal italic md:text-white text-primary">
                             <h2>Supported by :</h2>
                         </div>
                         
@@ -72,6 +77,41 @@ const Homepage = () => {
                             <img src="/assets/logo-mahasiswa-berdampak-circle.webp" alt="" className="w-10 " />
                             <img src="/assets/logo-mahasiswa-berdampak-box.webp" alt="" className="w-32" />
                         </div>
+                    </div>
+                </div>
+
+                {/* About Section*/}
+                <div id="tentang-kami" className="about-us flex flex-col md:flex-row items-center w-screen h-screen md:pt-28 px-30">
+                    <div className="box-about-1 flex flex-col items-center w-full md:w-1/2">
+                        <div className="flex flex-col gap-5 pt-15 px-10 w-screen md:w-full text-primary">
+                            <div className="flex flex-row gap-1">
+                                <div className="h-2 w-3 mb-5 bg-green-500 mt-2 rounded-full"></div> 
+                                <div className="h-2 w-12 mb-5 bg-green-500 mt-2 rounded-full"></div>
+                            </div>
+                            
+                            <h2 className="font-semibold text-3xl md:text-4xl italic">About</h2>
+                            <h1 className="font-bold text-4xl md:text-5xl"><span className="text-secondary">PM</span> BEM UNSRI?</h1>
+
+                            <p className="text-justify font-medium text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam nobis quam labore eaque quaerat error dicta molestias minima! 
+                                Nemo perspiciatis sed, esse tempora quas atque nisi repellendus iure dignissimos tempore nulla ab, modi asperiores iusto officiis, maiores omnis voluptatibus. 
+                                Suscipit nesciunt maxime inventore quam d   eserunt atque? Porro perferendis beatae voluptatum?
+                            </p>
+                        </div>
+
+                        <div className="flex flex-col md:flex-row justify-center items-center gap-5 p-10 w-screen md:w-full">
+                            <div className="flex flex-row items-center py-2 pl-4 pr-15 gap-3 w-fit text-left border-2 border-secondary rounded-2xl">
+                                <FiUsers className='p-2 w-12 h-12 text-white bg-linear-to-r from-[#16A34A] to-[#22C55E] rounded-md' />
+                                <h1 className="leading-7 font-bold italic text-2xl text-secondary">20+ <br /><span className="font-normal not-italic text-primary">Members</span></h1>
+                            </div>
+                            <div className="flex flex-row items-center py-2 pr-4 pl-15 gap-3 w-fit text-right border-2 border-secondary rounded-2xl">
+                                <h1 className="leading-7 font-bold italic text-2xl text-secondary">2 <br /><span className="font-normal not-italic text-primary">Programs</span></h1>
+                                <PiMedal className='p-2 w-12 h-12 text-white bg-linear-to-r from-[#16A34A] to-[#22C55E] rounded-md'/>
+                            </div>
+                        </div>  
+                    </div>
+
+                    <div className="box-about-2 flex items-center justify-center w-screen md:w-1/2">
+                        <img src="/assets/about.webp" alt="" className="w-sm md:w-lg" />
                     </div>
                 </div>
             </div>
